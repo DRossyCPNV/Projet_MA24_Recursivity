621681d50f16872a3f47b781b08588d96b020afe
David Rossy <Watermelon@mbpdedavidrossy-5.home>
2019-04-04 21:30:21 +0200

    Initialize project

---
+++ main.c
@@ -0,0 +1,27 @@
+//  PrimeNumber
+//
+//  Created by David Rossy on 04.04.19.
+//  Copyright © 2019 David_Rossy. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, const char * argv[]) {
+
+    int n = 0;
+    int control = 1;
+    //Title
+    printf("OPTIMUS PRIME ?\n");
+    printf("===============\n\n");
+    
+    //Tests (soon)
+    
+    //Asks a number to user
+    printf("Prime property of N = ");
+    scanf("%i", &n);
+    
+    return EXIT_SUCCESS;
+}
